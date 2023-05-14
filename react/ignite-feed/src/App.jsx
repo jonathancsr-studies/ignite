58b609ed620ea76a798f471952c2ae028b51e1a7
Jonathan Candido <jonathancsrr@gmail.com>
2023-05-14 18:27:41 -0300

    feat: initial commit

---
+++ react/ignite-feed/src/App.jsx
@@ -0,0 +1,14 @@
+import { Post } from "./Post"
+import { Header } from "./components/Header"
+import './global.css'
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Post author="jonathan" content="sjadhuahsihd iuashdui hasuihdu ihsaui dhsa" />
+    </>
+  )
+}
+
+export default App
