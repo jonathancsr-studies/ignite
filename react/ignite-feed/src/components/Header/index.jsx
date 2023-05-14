a6cb498a33436123338fb5575a439bda3c3ebd5a
Jonathan Candido <jonathancsrr@gmail.com>
2023-05-14 19:11:19 -0300

    fix: import

--- react/ignite-feed/src/components/Header/index.jsx
+++ react/ignite-feed/src/components/Header/index.jsx
@@ -1,4 +1,4 @@
-import igniteLogo from '../assets/ignite-logo.svg';
+import igniteLogo from '../../assets/ignite-logo.svg';
 import styles from './Header.module.css';
 export function Header() {
   return <header className={styles.header}>
