723b2c7de9af31c76f23955b512ac6b4ce6c3d38
Jonathan Candido <jonathancsrr@gmail.com>
2023-05-14 19:09:56 -0300

    chore: add global css

--- react/ignite-feed/src/App.jsx
+++ react/ignite-feed/src/App.jsx
@@ -2,11 +2,26 @@ import { Post } from "./Post"
 import { Header } from "./components/Header"
 import './global.css'
 
+import styles from './App.module.css'
+
 function App() {
   return (
     <>
       <Header />
-      <Post author="jonathan" content="sjadhuahsihd iuashdui hasuihdu ihsaui dhsa" />
+
+      <div className={styles.wrapper}>
+
+        <aside>Sidebar </aside>
+
+        <main>
+          <Post author="Jane Cooper" content="Fala galeraa 👋
+
+Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀" />
+          <Post author="jonathan" content="sjadhuahsihd iuashdui hasuihdu ihsaui dhsa" />
+
+        </main>
+
+      </div>
     </>
   )
 }
