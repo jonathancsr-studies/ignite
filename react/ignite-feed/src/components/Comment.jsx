853b4b5f070721435f345208090c778d9afcb172
Jonathan Candido <jonathancsrr@gmail.com>
2023-07-05 19:00:51 -0300

    feat: like comments

--- react/ignite-feed/src/components/Comment.jsx
+++ react/ignite-feed/src/components/Comment.jsx
@@ -1,12 +1,18 @@
-import { ThumbsUp, Trash } from 'phosphor-react'
-import { Avatar } from './Avatar'
-import styles from './Comment.module.css'
+import { ThumbsUp, Trash } from 'phosphor-react';
+import { useState } from 'react';
+import { Avatar } from './Avatar';
+import styles from './Comment.module.css';
 
 export function Comment({ content, onDeleteComment }) {
+  const [likeCount, setLikeCount] = useState(0);
+
   function handleDeleteComment() {
     onDeleteComment(content)
   }
 
+  function handleLikeComment() {
+    setLikeCount((state) => state + 1)
+  }
   return (
     <div className={styles.comment}>
       <Avatar hasBorder={false} src="https://github.com/jonathancsr.png" />
@@ -24,9 +30,9 @@ export function Comment({ content, onDeleteComment }) {
           <p>{content}</p>
         </div>
         <footer>
-          <button>
+          <button onClick={handleLikeComment}>
             <ThumbsUp />
-            Aplaudir <span>20</span>
+            Aplaudir <span>{likeCount}</span>
           </button>
         </footer>
       </div>
