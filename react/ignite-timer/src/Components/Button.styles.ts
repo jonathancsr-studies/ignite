4d9fc197d4c736675e399182789c5feb3a66ff84
Jonathan Candido <jonathan.candido@valwy.com>
2023-08-27 16:03:38 -0300

    styles: apply

--- react/ignite-timer/src/Components/Button.styles.ts
+++ react/ignite-timer/src/Components/Button.styles.ts
@@ -16,7 +16,12 @@ const buttonVariants = {
 export const ButtonContainer = styled.button<ButtonContainerProps>`
   width: 100px;
   height: 40px;
-  background-color: ${(props) => props.theme.secondary};
+  border-radius: 4px;
+  border: 0;
+  margin: 8px;
+
+  background-color: ${(props) => props.theme["green-500"]};
+  color: ${(props) => props.theme.white};
   /* ${({ variant }) =>
     css`
       background-color: ${buttonVariants[variant]};
