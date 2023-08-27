0352415904793fe4d925262136c869a020274bd4
Jonathan Candido <jonathan.candido@valwy.com>
2023-08-27 15:23:19 -0300

    feat: initial commit

---
+++ react/ignite-timer/src/App.tsx
@@ -0,0 +1,13 @@
+import { Button } from "./Components/Button";
+
+export function App() {
+  return (
+    <>
+      <Button />
+      <Button variant="primary" />
+      <Button variant="secondary" />
+      <Button variant="danger" />
+      <Button variant="success" />
+    </>
+  );
+}
