09d820bbd69250987f08b883a3f7157432c5bf55
jonathancsr <jonathancsrr@gmail.com>
2024-01-23 16:06:45 -0300

    chore: add input style

--- react/ignite-timer/src/pages/Home/styles.ts
+++ react/ignite-timer/src/pages/Home/styles.ts
@@ -28,6 +28,38 @@ export const FormContainer = styled.div`
   flex-wrap: wrap;
 `;
 
+const BaseInput = styled.input`
+  background-color: transparent;
+  color: ${(props) => props.theme["gray-100"]};
+  height: 2.5rem;
+  border: none;
+  border-bottom: 2px solid ${(props) => props.theme["gray-500"]};
+  font-weight: bold;
+  font-size: 1.125rem;
+  padding: 0 0.5rem;
+
+  &:focus {
+    box-shadow: none;
+    border-color: ${(props) => props.theme["green-500"]};
+  }
+
+  &::placeholder {
+    color: ${(props) => props.theme["gray-500"]};
+  }
+`;
+
+export const TaskInput = styled(BaseInput)`
+  flex: 1;
+
+  &::-webkit-calendar-picker-indicator {
+    display: none !important;
+  }
+`;
+
+export const MinutesAmountInput = styled(BaseInput)`
+  width: 4rem;
+`;
+
 export const CountdownContainer = styled.div`
   font-family: "Roboto Mono", monospace;
   font-size: 10rem;
@@ -53,4 +85,34 @@ export const Separator = styled.div`
 
   display: flex;
   justify-content: center;
+`;
+
+export const StartCountdownButton = styled.button`
+  width: 100%;
+  border: none;
+  padding: 1rem;
+  border-radius: 8px;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  gap: 0.5rem;
+  font-weight: bold;
+
+  cursor: pointer;
+
+  background: ${(props) => props.theme["green-500"]};
+  color: ${(props) => props.theme["gray-100"]};
+  transition: background 0.2s;
+
+  &:disabled {
+    background: ${(props) => props.theme["gray-500"]};
+    color: ${(props) => props.theme["gray-300"]};
+    cursor: not-allowed;
+  }
+
+  &:not(:disabled):hover {
+    background: ${(props) => props.theme["green-700"]};
+  }
 `;
